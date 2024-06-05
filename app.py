bad6463dae54a2cc5eefbd70aa953c21f365c912
JamesB15code <129150534+JamesB15Code@users.noreply.github.com>
2024-06-05 15:38:04 -0600

    se agrego un template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask (__name__)
 
 @app.route('/')
-def home():
-    return "Hola mundo"
+def form():
+    return render_template('inicio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
